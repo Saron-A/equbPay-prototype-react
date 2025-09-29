4d00d9b0cd7a8be39dfaeeee0fee9a78418b2679
Saron <saronabebe2019@gmail.com>
2025-09-29 14:03:42 -0800

    Edit homepage catch-phrase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1>Welcome to EqubPay</h1>
-      <p>Keep track of your turn to contribute and win!</p>
+      <p>Your Contribution, Your Turn, Your Prosperity!</p>
       <img src={Image} alt="" height="200px" width="300px" />
     </div>
   );
