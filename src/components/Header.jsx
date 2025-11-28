ec76faf14ba8034923b4b54162ec1ffb531bcaa7
Saron <saronabebe2019@gmail.com>
2025-11-28 12:15:26 -0800

    Remove unnecessary stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,6 @@ import Image from "../assets/images/header-img.jpg";
 const Header = () => {
   return (
     <div className="header">
-      <nav>
-        <ul>
-          <li>
-            {" "}
-            <a href="http://localhost:4000/login">Login</a>
-          </li>
-          <li>
-            <a href="/profile">Profile</a>
-          </li>
-        </ul>
-      </nav>
       <h1>Welcome to EqubPay</h1>
       <p>Your Contribution, Your Turn, Your Prosperity!</p>
       <img src={Image} alt="" height="200px" width="300px" />
